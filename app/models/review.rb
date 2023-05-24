9e5a24f1e89d7ec527687cbc38483b24be3b0448
marta vasallo <mvasalloperez@gmail.com>
2023-05-24 14:31:55 +0100

    Yelp app

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5) }, numericality: { only_integer: true }
 end
