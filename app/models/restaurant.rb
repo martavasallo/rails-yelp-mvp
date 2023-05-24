9372c76388baa8bc4ac3de1a83225f4dbdbf1c71
marta vasallo <mvasalloperez@gmail.com>
2023-05-24 12:19:10 +0100

    changed phone number data type

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ApplicationRecord
+  
+  has_many :reviews, dependent: :destroy
+
+  CATEGORIES = %w[chinese italian japanese french belgian]
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone_number, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+
 end
